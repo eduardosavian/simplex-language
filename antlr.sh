11778f522c10a9267d7f94e178b50b75ddac20b9
Eduardo Savian <eduardosavian@outlook.com>
2024-03-30 16:21:19 -0300

    Changes antlr.sh

--- antlr.sh
+++ antlr.sh
@@ -2,8 +2,11 @@
 
 # Get the full path to the antlr4.jar file
 antlr4_path="tools/antlr4.jar"
+
 antlr4_full_path=$(realpath "$antlr4_path")
 
-echo $antlr4_full_path
+
+simplex_path="src/Simplex.g4"
+
 # Run the ANTLR 4 tool
-java -cp "$antlr4_full_path:$CLASSPATH" org.antlr.v4.Tool $1
+java -cp "$antlr4_full_path:$CLASSPATH" org.antlr.v4.Tool $simplex_path
