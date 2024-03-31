220c61ea1a2b80dda3f6ee9c3e7289d545a42bd6
Eduardo Savian <eduardosavian@outlook.com>
2024-03-31 15:55:16 -0300

    Add args prints

--- src/Simplex.java
+++ src/Simplex.java
@@ -1,14 +1,25 @@
 public class Simplex {
     public static void main(String[] args) throws Exception {
-        if (args.length != 1) {
-            System.err.println("Usage: java Main <input_file>");
+        if (args.length != 2) {
+            System.err.println("Usage: java Main <input_file> <print_type>");
             System.exit(1);
         }
 
         Utils utils = new Utils();
 
         String inputFilePath = args[0];
+        String printType = args[1];
 
-        utils.printNormal(inputFilePath);
+        switch (printType) {
+            case "normal":
+                utils.printNormal(inputFilePath);
+                break;
+            case "tree":
+                utils.printTree(inputFilePath);
+                break;
+            default:
+                System.err.println("Invalid print type. Supported types: normal, formatted");
+                System.exit(1);
+        }
     }
 }
