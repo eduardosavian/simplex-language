bc39d0ab8de1630293136aed8ec39b34fa87f9f8
Eduardo Savian <eduardosavian@outlook.com>
2024-05-02 17:15:35 -0300

    Add functions

--- src/Semantic.java
+++ src/Semantic.java
@@ -1,3 +1,35 @@
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import java.lang.reflect.Method;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.reflect.Method;
+
+import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.tree.*;
+
+
 public class Semantic {
-    
+    public void execute(String inputFilePath) throws IOException {
+        InputStream inputStream = new FileInputStream(inputFilePath);
+
+        @SuppressWarnings("deprecation")
+        ANTLRInputStream input = new ANTLRInputStream(inputStream);
+
+        SimplexLexer lexer = new SimplexLexer(input);
+
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+
+        SimplexParser parser = new SimplexParser(tokens);
+
+        ParseTree tree = parser.program();
+
+        SimplexVisitor simplex = new SimplexVisitor();
+
+        simplex.visit(tree);
+    }
 }
