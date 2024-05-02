19c8bff8b7bc656163aab3713bdd16cf41d9d458
Eduardo Savian <eduardosavian@outlook.com>
2024-05-02 20:57:00 -0300

    Reformating

--- src/Sintatic.java
+++ src/Sintatic.java
@@ -33,23 +33,15 @@ public class Sintatic {
 
     private void antlrCommand(String inputFilePath, PrintFlag printType) {
         try {
-            
-            
-            // String[] argArray = args.split("\\s+");
+            String[] fullArgs = new String[4];
+            fullArgs[0] = "Simplex";
+            fullArgs[1] = "program";
+            fullArgs[2] = inputFilePath;
+            fullArgs[3] = printType.getFlag();
 
-            // String[] fullArgs = new String[argArray.length + 3];
-            // fullArgs[0] = "Simplex";
-            // fullArgs[1] = "program";
-            // fullArgs[2] = inputFilePath;
-            // System.arraycopy(argArray, 0, fullArgs, 3, argArray.length);
-
-            // for (String arg : fullArgs) {
-            //     System.out.println(arg);
-            // }
-
-            // Class<?> testRigClass = Class.forName("org.antlr.v4.gui.TestRig");
-            // Method mainMethod = testRigClass.getMethod("main", String[].class);
-            // mainMethod.invoke(null, new Object[] { fullArgs });
+            Class<?> testRigClass = Class.forName("org.antlr.v4.gui.TestRig");
+            Method mainMethod = testRigClass.getMethod("main", String[].class);
+            mainMethod.invoke(null, new Object[] { fullArgs });
         } catch (Exception e) {
             System.out.println("");
         }
